d0d81efedf1f8e09e8a925ac298128a4df06f534
Edgar Kalkowski <kalkowski@uni-kassel.de>
2014-09-09 16:49:31 +0200

    Print remote CPU info.

--- dzen.cpp
+++ dzen.cpp
@@ -3,11 +3,43 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <sstream>
 #include <thread>
 #include <chrono>
+#include <experimental/optional>
 
 using namespace std;
+using experimental::optional;
+
+class log final {
+
+public:
+
+  template<typename T>
+  log& operator<<(const T& text) {
+#ifdef DEBUG
+    buffer << text;
+#endif
+    return *this;
+  }
+
+  typedef basic_ostream<char, char_traits<char>> endl_t;
+  log& operator<<(endl_t&(*endl)(endl_t&)) {
+#ifdef DEBUG
+    clog << "\033[30m" << buffer.str() << "\033[m" << endl;
+    buffer.str("");
+    buffer.clear();
+#endif
+    return *this;
+  }
+
+private:
+  stringstream buffer;
+
+};
+
+log debug;
 
 struct cpu {
   long user;
@@ -101,49 +133,111 @@ cpu_info read_cpu_info(string filename) {
   return cpu_info{total, cpus};
 }
 
-cpu_info read_remote_cpu_info(const string& remote) {
+optional<cpu_info> read_remote_cpu_info(const string& remote) {
   string filename("/tmp/" + remote + ".proc_stat");
-  system(string("scp " + remote + ":/proc/stat " + filename + " &>/dev/null").c_str());
-  cpu_info result = read_cpu_info(filename);
-  remove(filename.c_str());
-  return result;
+  const int result = system(string("ssh " + remote + " 'cat /proc/stat' > " + filename + " 2>/dev/null").c_str());
+
+  if (result == 0) {
+    cpu_info info = read_cpu_info(filename);
+    remove(filename.c_str());
+    return optional<cpu_info>(info);
+  } else {
+    return optional<cpu_info>();
+  }
 }
 
 class dzen final {
 
 public:
 
-  dzen(const int width, const vector<string> remotes) : width(width), remotes(remotes) {
+  dzen(const int width, const vector<string> hosts) : width(width), hosts(hosts), padding(calc_padding(hosts)) {
   }
 
   void run() {
-    bool initialized = false;
+    int steps = 0;
+    const int limit = 10;
+    chrono::steady_clock::time_point start;
+    chrono::milliseconds offset(0);
     cpu_info before(read_cpu_info("/proc/stat"));
-    vector<pair<string, cpu_info>> remote;
+    map<string, optional<cpu_info>> remotes;
+    map<string, optional<cpu_info>> remote_diffs;
 
-    for (string host : remotes) {
-      remote.push_back(make_pair(host, read_remote_cpu_info(host)));
+    for (const string host : hosts) {
+      remotes[host] = read_remote_cpu_info(host);
     }
 
     while (true) {
-      this_thread::sleep_for(chrono::seconds(1));
+      chrono::milliseconds sleep = chrono::seconds(1) - offset;
+      this_thread::sleep_for(sleep);
+      start = chrono::steady_clock::now();
+      steps++;
 
-      // Process local CPU information.
+      // Update local CPU information.
       cpu_info after(read_cpu_info("/proc/stat"));
       cpu_info diff(after - before);
-      cout << "CPUs";
+      before = after;
+      cout << "^tw()CPUs";
 
       for (auto cpu : diff.cpus) {
-        cout << " " << print(cpu);
+        cout << " " << print(cpu, static_cast<long>(0.75*width));
       }
 
       cout << endl;
+
+      // Update remote CPU information.
+      if (steps >= limit || remote_diffs.empty()) {
+        steps = 0;
+
+        for (const string host : hosts) {
+          optional<cpu_info> now(read_remote_cpu_info(host));
+
+          if (now && remotes[host]) {
+            cpu_info n = now.value_or(cpu_info());
+            cpu_info b = remotes[host].value_or(cpu_info());
+            remote_diffs[host] = optional<cpu_info>(n - b);
+          } else {
+            remote_diffs.erase(host);
+          }
+
+          if (now) {
+            remotes[host] = now;
+          }
+        }
+
+        stringstream buffer;
+        buffer << "^cs()" << endl;
+
+        for (const string host : hosts) {
+          buffer << host << ": ";
+
+          for (uint i = 0; i < padding - host.length(); i++) {
+            buffer << " ";
+          }
+
+          if (remote_diffs.find(host) != remote_diffs.end()) {
+            buffer << "CPU " << print(remote_diffs[host].value_or(cpu_info()).total, 2 * width) << endl;
+          } else {
+            buffer << "<unreachable>" << endl;
+          }
+        }
+
+        cout << buffer.str();
+      }
+
+      offset = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start);
+
+      if (offset.count() == 0) {
+        debug << "Update took < 1 ms." << endl;
+      } else {
+        debug << "Update took " << offset.count() << " ms." << endl;
+      }
     }
   }
 
 private:
   const int width;
-  const vector<string> remotes;
+  const vector<string> hosts;
+  const int padding;
 
   static const int HEIGHT = 10;
 
@@ -156,12 +250,33 @@ private:
   static const string GREY;
   static const string DARK_GREY;
 
-  inline void p(ostream& stream, const long& value, const string& colour) {
+  static int calc_padding(const vector<string>& hosts) {
+    int max_length = 0;
+
+    for (string host : hosts) {
+      int length = host.length();
+
+      if (length > max_length) {
+        max_length = length;
+      }
+    }
+
+    return max_length;
+  }
+
+  static inline void p(ostream& stream, const long& value, const string& colour) {
     stream << "^fg(" << colour << ")^r(" << value << "x" << HEIGHT << ")";
   }
 
-  string print(const cpu& c) {
-    cpu n = normalize(c);
+  /**
+   * Format a CPU info instance according to the format dzen2 expects.
+   *
+   * @param cpu  CPU info instance to be formatted.
+   * @param width  Width in pixels in which the specified CPU info instance shall be visualized.
+   * @return  A string that contains output suitable for dzen2.
+   */
+  static string print(const cpu& c, const long width) {
+    cpu n = normalize(c, width);
     stringstream s;
     p(s, n.nice, BLUE);
     p(s, n.user, GREEN);
@@ -176,11 +291,18 @@ private:
     return s.str();
   }
 
-  inline long sum(const cpu& cpu) const {
+  static inline long sum(const cpu& cpu) {
     return cpu.user + cpu.nice + cpu.system + cpu.idle + cpu.iowait + cpu.irq + cpu.softirq + cpu.steal + cpu.guest + cpu.guest_nice;
   }
 
-  cpu normalize(const cpu& c) const {
+  /**
+   * Normalize a CPU info instance to a desired display width.
+   *
+   * @param c  CPU info instance to be normalized.
+   * @param width  Width in pixels the given CPU info instance shall be normalized to.
+   * @return  A new CPU info instance that is normalized in such a way that the sum of all it’s entries equals the specified width.
+   */
+  static cpu normalize(const cpu& c, const long& width) {
     cpu result;
     const long s = sum(c);
     result.user = width * c.user / s;
@@ -219,7 +341,7 @@ int main(int argc, char** argv) {
   long width;
   buffer >> width;
 
-  vector<string> remotes{"pallando", "alatar"};
+  vector<string> remotes{"pallando", "alatar", "radagast", "sauron", "ulmo", "irmo", "melko"};
   dzen(width, remotes).run();
 
   return EXIT_SUCCESS;
